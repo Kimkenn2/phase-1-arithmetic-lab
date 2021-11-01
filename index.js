c9bf53c44840b06c2f1c5471ca67a18a79b24822
Kenneth Kim <kkm2059@gmail.com>
2021-11-01 12:52:14 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = num1*num2
+//
+const random = Math.floor(Math.random() * 10) + 1;
+//
+const num3 = 24
+const num4 = 6
+const mod = num3/num4
+//
+const max = Math.max(4,2,20,5,1,4,15,19)
